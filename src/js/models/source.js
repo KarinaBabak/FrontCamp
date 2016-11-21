5f8b8ea782a64f8c44a209b49ddc5824bee37d40
KarinaBabak <karisha.babak@gmail.com>
2016-11-22 01:33:53 +0300

    refactoring: add classes Article, Source

---
+++ src/js/models/source.js
@@ -0,0 +1,12 @@
+class Source {
+    constructor(id, name, description, category, urlsToLogos) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.urlsToLogos = urlsToLogos;
+    }
+
+ 
+
+};
---
+++ src/js/models/sourceHeading.js
@@ -0,0 +1,8 @@
+class SourceHeading {
+    constructor() {
+        
+    }
+
+ 
+
+};
