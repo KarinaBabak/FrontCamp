62e56022419a15b9b7e7e7403f8c27db1a17a835
Aliaksei Demukh <aliaksei_demukh@epam.com>
2017-01-06 11:22:36 +0300

    add ui for reading article

--- controllers/picture.js
+++ controllers/picture.js
@@ -1,6 +1,6 @@
 module.exports = {
     getImgPath: function(file) {
-        return file.destination + '/' 
+        return file.destination.split('/')[1] + '/' 
             + file.filename + '.' 
             + file.mimetype.split('/')[1];
     }    
